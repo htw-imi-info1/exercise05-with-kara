649f935dce4b4a8d4dcef376957b8845ca7dcfd1
B Kleinen <kleinen@infrastructure.de>
2013-11-28 10:23:35 +0100

    solution in lab

--- kara-copy/CopyKara.java
+++ kara-copy/CopyKara.java
@@ -12,15 +12,57 @@ public class CopyKara extends Kara
      *
      *
      */
- 
+
     public void act() 
     {
-        ArrayList<Boolean> leafRow = new ArrayList<Boolean>();
-        turnLeft();
+
+        ArrayList<Boolean> leafRow = checkLeft();
+        layRight(leafRow);
+        if (treeFront()){
+            Greenfoot.stop();
+            return;
+        }
+        move();
+
         // walk to the left and store in the leafRow variable wether you found a leaf or not
         // walk back to middle
         // walk to the right and use leafRow to decide wether to put a leaf or not
-      
+
     }
 
+    public  ArrayList<Boolean> checkLeft(){
+        turnLeft();
+        ArrayList<Boolean> leafRow = new ArrayList<Boolean>();
+        while(!treeFront()){
+            move();
+            leafRow.add(onLeaf());
+            /* ersetzt: 
+            if(onLeaf())
+                leafRow.add(true);
+            else
+                leafRow.add(false);
+                */
+                
+        }
+        walkBack(leafRow.size());
+
+        turnLeft();
+        return leafRow;
+    }
+
+    public void layRight( ArrayList<Boolean> leafRow){
+        turnRight();
+        for(Boolean leafHere : leafRow){
+            move();
+            if(leafHere)
+                putLeaf();
+        }
+        walkBack(leafRow.size());
+        turnRight();
+
+    }
+
+    public void walkBack(int length){
+        turnLeft();turnLeft();
+        move(length);}
 }
