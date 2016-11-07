20a8399a27ba459ec51a2e7629569e89bd674e60
B Kleinen <kleinen@infrastructure.de>
2016-11-07 16:23:42 +0100

    added Class with empty methods for prelab

---
+++ prelab-and-primes/PreLab.java
@@ -0,0 +1,41 @@
+import java.util.*;
+/**
+ * Fill in your solutions for the PreLab in
+ * the corresponding methods.
+ * 
+ * @author Barne Kleinen
+ */
+public class PreLab
+{
+    public void printMultiplesOf5()
+    {
+
+    }
+
+    public int sumBetween(int a, int b){
+        return -1;
+    }
+
+    public void printStudentList(Collection<Student> imi1)
+    {
+    }
+    
+    public boolean isPrime(int n){
+        return false;
+    }
+    
+    public int countPrimesSmallerThan1000(){
+        return 0;
+    }
+    class Student{
+        String firstName, surname;
+        Student(String firstName, String surname){
+            this.firstName = firstName; 
+            this.surname = surname;
+        }
+
+        String getFirstName(){return firstName;}
+
+        String getSurname(){return surname;}
+    }
+}
